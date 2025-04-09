12821156c1f7c5ce95b10a8b7cb6cd55f1a69276
Krishna20Solanki <143687596+Krishna20Solanki@users.noreply.github.com>
2025-04-09 15:36:01 +0530

    Update custom.ts

--- src/axios/custom.ts
+++ src/axios/custom.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://fashion-ecommerce-shop-in-react.onrender.com",
     headers: {
         Accept: "application/json"
     }
